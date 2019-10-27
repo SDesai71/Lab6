c1b4fec63bb470b78702cc3c53abb772f936b8be
Sahil Desai <sdesai10@my.bcit.ca>
2019-10-27 16:15:01 -0700

    Update TranslationsTest.php

--- tests/App/Language/TranslationsTest.php
+++ tests/App/Language/TranslationsTest.php
@@ -18,7 +18,7 @@ class TranslationsTest extends TestCase
 	 */
 	public function translations()
 	{
-		$dir = getcwd() . '/Lab6/app/Language/';
+		$dir = getcwd() . "'/app/Language/'";
 		$dh = opendir($dir);
 		$sets = [];
 		while (($file = readdir($dh)) !== false)
@@ -142,7 +142,7 @@ class TranslationsTest extends TestCase
 	 */
 	public function foundSets($locale)
 	{
-		$dir = getcwd() . '/Language/' . $locale . '/';
+		$dir = getcwd() . "'/app/Language/'" . $locale . '/';
 		$dh = opendir($dir);
 		$sets = [];
 		while (($file = readdir($dh)) !== false)
@@ -162,7 +162,7 @@ class TranslationsTest extends TestCase
 	 */
 	public function loadKeys($which, $locale = null)
 	{
-		$folder = $locale ? getcwd() . '/Language/' . $locale . '/' :
+		$folder = $locale ? getcwd() . "'/app/Language/'" . $locale . '/' :
 				getcwd() . 'app/Language/en';
 		$file = $folder . $which;
 		$keys = require $file;
